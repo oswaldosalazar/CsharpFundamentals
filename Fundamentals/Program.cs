90df3bb43afb97efe14a0edc7a4ae2b9ee46ca76
oswaldosalazar <oswaldo.r.salazar@gmail.com>
2017-05-11 19:52:14 -0600

    Initial commit

---
+++ Fundamentals/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Fundamentals
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int myAge = 22;
+            int yourAge = 35;
+
+            myAge = 23;
+            // retirement = 72;
+
+            const int retirement = 65;
+
+            int yearsToRetirementForMe = retirement - myAge;
+            int yearsToRetirementForYou = retirement - yourAge;
+
+            string message = "Will we be able to retire soon?";
+
+            Console.WriteLine(message);
+            Console.WriteLine("Years to retirement for you: " + yearsToRetirementForYou);
+            Console.WriteLine("Years to retirement for me: {0}", yearsToRetirementForMe);
+        }
+    }
+}
